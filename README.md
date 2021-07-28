64056d2ebf22e9afcf6476178fe163b3aae3daee
David Kajpust <kajpustd@gmail.com>
2021-07-28 13:31:20 -0400

    Initial commit. Took code from https://github.com/davekaj/scrape-snapshot

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# snapshot-governance
-Pulls IPFS files from Snapshot and pushes them to our own IPFS node.
+# Snapshot Governance
+Scrapes snapshot to pin the IPFS files to your own node.
+
+# How it works
+
+- `scrape.js` hits snapshots API to get all the proposals
+- It writes the files proposals.txt and proposals.relayers.txt to use for `ipfs-sync`
+- It then gets all the votes from the proposals from snapshots api
+- It writes the files votes.txt and votes-relayers.txt
+- It then runs `ipfs-sync` on these four files
+- All the scripts are de-dupped so that this could run on a VM in the cloud, ping it once a day,
+  and it will only append to the txt files. Then ipfs-sync already skips over dupes.
+
+# TODO 
+
+- Currently written to pull from balancers snapshot API and not ours, beacuse this repo is from
+  February, and we did not have governance done.
+- Bug - Get `ipfs-sync` to not boot out after batching the first 10 files to be synced over. Some reason
+  it randomly boots out of the process
+- Pin to multiple IPFS gateways
+- Figure out how to set it up so that it is a process that runs in the cloud daily. Maybe part of
+  our monitoring system.
---
+++ src/scrape.js
@@ -0,0 +1,100 @@
+const axios = require("axios");
+const fs = require("fs");
+
+async function getProposals() {
+  try {
+    // This returns a JSON file from the snapshot hub, NOT an actual IPFS file stored on an IPFS node
+    // It contains all proposals. Therefore we take the IPFS files from this list and query a public gateway
+    // Snapshot posts their IPFS files to many public nodes, list is here https://github.com/snapshot-labs/snapshot.js/blob/master/src/gateways.json
+    const response = await axios.get(
+      "https://hub.snapshot.page/api/balancer/proposals"
+    );
+    const data = response.data;
+
+    for (const proposal in data) {
+      const relayerIpfsHash = data[proposal].relayerIpfsHash;
+      const proposals = fs.readFileSync("./data/proposals.txt", "utf8");
+      let duplicateProposal = proposals.includes(proposal);
+      if (duplicateProposal) {
+        console.log(`Duplicate proposal    : ${proposal}`);
+      } else {
+        fs.appendFileSync("./data/proposals.txt", proposal + "\n");
+        console.log(`Added proposal        : ${proposal} to proposals.txt`);
+      }
+
+      const relayers = fs.readFileSync("./data/proposals-relayers.txt", "utf8");
+      let duplicateRelayerIpfsHash = relayers.includes(relayerIpfsHash);
+      if (duplicateRelayerIpfsHash) {
+        console.log(`Duplicate relayer hash: ${relayerIpfsHash}`);
+      } else {
+        fs.appendFileSync(
+          "./data/proposals-relayers.txt",
+          relayerIpfsHash + "\n"
+        );
+        console.log(
+          `Added relayer hash    : ${relayerIpfsHash} to proposals-relayers.txt`
+        );
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getVotes() {
+  try {
+    let proposals = fs.readFileSync("./data/proposals.txt", "utf8").split("\n");
+    proposals.pop(); // remove final new line
+    console.log(proposals);
+
+    // Here  we query for the proposal votes
+    // Note - if no votes have been created, it will just return an empty object
+    for (const proposal of proposals) {
+      const response = await axios.get(
+        `https://hub.snapshot.page/api/balancer/proposal/${proposal}`
+      );
+      const data = response.data;
+
+      // Once we grab the proposal, from this end point, we need to parse out all of the
+      // IPFS hashes, and add them to a txt file for ipfs-sync
+      for (const vote in data) {
+        // For the votes we must get the authorIpfsHash and relayerIpfsHash
+        // These files have stored data to relate them back to the proposal, so no need to store special information
+        const authorIpfsHash = data[vote].authorIpfsHash;
+        const relayerIpfsHash = data[vote].relayerIpfsHash;
+
+        const votes = fs.readFileSync("./data/votes.txt", "utf8");
+        let duplicateVote = votes.includes(authorIpfsHash);
+        if (duplicateVote) {
+          console.log(`Duplicate vote        : ${authorIpfsHash}`);
+        } else {
+          fs.appendFileSync("./data/votes.txt", authorIpfsHash + "\n");
+          console.log(`Added vote            : ${proposal} to votes.txt`);
+        }
+
+        const relayers = fs.readFileSync("./data/votes-relayers.txt", "utf8");
+        let duplicateRelayerIpfsHash = relayers.includes(relayerIpfsHash);
+        if (duplicateRelayerIpfsHash) {
+          console.log(`Duplicate relayer hash: ${relayerIpfsHash}`);
+        } else {
+          fs.appendFileSync(
+            "./data/votes-relayers.txt",
+            relayerIpfsHash + "\n"
+          );
+          console.log(
+            `Added relayer hash    : ${relayerIpfsHash} to votes-relayers.txt`
+          );
+        }
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function scrapeSnapshot() {
+  await getProposals();
+  await getVotes();
+}
+
+scrapeSnapshot();
